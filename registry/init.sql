190b0780863909b23fd173b8a2be5132f3d02ea1
Doug Davis <dug@microsoft.com>
2024-01-18 16:45:32 +0000

    don't copy 'latest' from the version
    Signed-off-by: Doug Davis <dug@microsoft.com>

--- registry/init.sql
+++ registry/init.sql
@@ -237,7 +237,7 @@ JOIN Versions AS v ON (p.EntitySID=v.SID)
 JOIN Resources AS r ON (r.SID=v.ResourceSID)
 JOIN Props AS p1 ON (p1.EntitySID=r.SID)
 WHERE p1.PropName='latestVersionId,' AND v.UID=p1.PropValue AND
-      p.PropName<>'id,' AND p.PropName<>'latest';     # Don't overwrite these
+      p.PropName<>'id,' AND p.PropName<>'latest,';     # Don't overwrite these
 # NOTE!!! if DB_IN changes then the above 2 lines MUST change
 # TODO move the creation of this into the code then we can dynamically
 # use DB_IN instead of hard-coding the "," in here
