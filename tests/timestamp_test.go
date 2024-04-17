7a5acecfb47084b358b0d33868a5bb335c58e3ce
Doug Davis <dug@microsoft.com>
2024-04-17 17:50:08 +0000

    regex fix
    Signed-off-by: Doug Davis <dug@microsoft.com>

--- tests/timestamp_test.go
+++ tests/timestamp_test.go
@@ -40,7 +40,7 @@ func TestTimestampRegistry(t *testing.T) {
 		Method: "GET",
 		Code:   200,
 		BodyMasks: []string{
-			`[0-4]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?Z|YYYY-MM-DDTHH:MM:SSZ`,
+			`[0-4]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?[^"]*|YYYY-MM-DDTHH:MM:SSZ`,
 		},
 		ResBody: `{
   "specversion": "` + registry.SPECVERSION + `",
