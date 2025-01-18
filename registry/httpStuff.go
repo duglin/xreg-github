82988c20564d298f352189693761957bdb3c7939
Doug Davis <duglin@gmail.com>
2025-01-18 02:08:40 +0000

    tweak ui
    Signed-off-by: Doug Davis <duglin@gmail.com>

--- registry/httpStuff.go
+++ registry/httpStuff.go
@@ -582,11 +582,13 @@ func (pw *PageWriter) Done() {
     margin: 0 ;
   }
   #left {
-    padding: 8 20 8 8 ;
+    padding: 0 0 25 0 ;
     background-color: #e3e3e3 ; // gainsboro ; // lightsteelblue;
     white-space: nowrap ;
     overflow-y: auto ;
     min-width: fit-content ;
+    display: table ;
+    border-right: 2px solid lightgray ;
   }
   #right {
     display: flex ;
@@ -612,8 +614,14 @@ func (pw *PageWriter) Done() {
   #registry {
     display: flex ;
     align-items: baseline ;
-    margin-right: -10 ;
-    margin-top: 2 ;
+    padding: 5 10 5 5 ;
+    background: white ;
+  }
+
+  #options {
+    display: flex ;
+    flex-direction: column ;
+    padding: 5 5 5 5 ;
   }
 
   #xRegLogo {
@@ -692,11 +700,11 @@ func (pw *PageWriter) Done() {
     background: #407d16 ;
     border: 1px solid #407d16 ;
     border-radius: 13px ;
-	color: white ;
+    color: white ;
     font-weight: bold ;
-    margin-left: 3px ;
+    margin-left: 10px ;
     padding: 2 10 3 10 ;
-	align-self: center
+    align-self: center
   }
 
   select:hover { background: #c4c4c4 ; color : black ; }
@@ -832,13 +840,13 @@ func (pw *PageWriter) Done() {
     <b>egistry:</b>
     <select onchange="changeRegistry(value)">`+list+`    </select>
   </div>
-  <hr style="margin-right:-15">
+  <div id=options>
 `+roots+`
   <div id=buttonList>
     `+applyBtn+`
     `+detailsButton+`
   </div>
-  <br style="height:20px">  <!-- buffer for octocat logo -->
+  </div> <!-- options -->
 </div>  <!-- left -->
 
 <script>
@@ -943,7 +951,7 @@ function dokeydown(event) {
   </div><div id='text'>%s</div></div> <!-- myOutput -->
 </div> <!-- right -->
 
-<svg height="20" aria-hidden="true" viewBox="0 0 24 24" width="20" style="position:absolute;left:0;bottom:0;cursor:pointer;margin:5" onclick="opensrc('commit')">
+<svg height="20" aria-hidden="true" viewBox="0 0 24 24" width="20" style="position:fixed;left:0;bottom:0;cursor:pointer;margin:5" onclick="opensrc('commit')">
   <title>Open commit: `+GitCommit+`</title>
   <path d="M12.5.75C6.146.75 1 5.896 1 12.25c0 5.089 3.292 9.387 7.863 10.91.575.101.79-.244.79-.546 0-.273-.014-1.178-.014-2.142-2.889.532-3.636-.704-3.866-1.35-.13-.331-.69-1.352-1.18-1.625-.402-.216-.977-.748-.014-.762.906-.014 1.553.834 1.769 1.179 1.035 1.74 2.688 1.25 3.349.948.1-.747.402-1.25.733-1.538-2.559-.287-5.232-1.279-5.232-5.678 0-1.25.445-2.285 1.178-3.09-.115-.288-.517-1.467.115-3.048 0 0 .963-.302 3.163 1.179.92-.259 1.897-.388 2.875-.388.977 0 1.955.13 2.875.388 2.2-1.495 3.162-1.179 3.162-1.179.633 1.581.23 2.76.115 3.048.733.805 1.179 1.825 1.179 3.09 0 4.413-2.688 5.39-5.247 5.678.417.36.776 1.05.776 2.128 0 1.538-.014 2.774-.014 3.162 0 .302.216.662.79.547C20.709 21.637 24 17.324 24 12.25 24 5.896 18.854.75 12.5.75Z"></path>
 </svg>
